8e8a127d8cd2ce6a58c9960721fed4dfb155a380
chipseater <jeanpauldemalet@yahoo.fr>
2022-04-12 09:10:17 +0000

    [UI] Dynamic navbar done

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
     <header class="shadow1">
         <RouterLink to="/" class="banner">
             <img src="https://via.placeholder.com/64" width="64" height="64">
-            <h2 v-if="windowWidth > 1250">Les mathématiques à SJH</h2>
+            <h2 v-if="windowWidth > 1250">Les mathématiques à SJH {{ currentRouteName }} </h2>
         </RouterLink>
         <div class="search">
             <button class="material-icons white">
@@ -11,18 +11,18 @@
             <input autocomplete="off" name="search-input" class="search-input" placeholder="Rechercher">
         </div>
         <nav>
-            <RouterLink to="/browse" class="navbar-link" :class="currentRouteName == '' ? '':''">Parcourir</RouterLink>
+            <RouterLink to="/browse" class="navbar-link" :class="currentRouteName == 'browse' ? 'active' : ''">Parcourir</RouterLink>
             <a v-if="currentRouteName == 'home'" href="#levels" class="navbar-link">Niveaux</a>
             <RouterLink
                 v-if="connected"
                 to="/profile" 
                 class="navbar-link" 
                 :class="currentRouteName == 'profile' ? '' : 'active'">
-                Mon compte
+                Mon compte {{ currentRouteName }}
             </RouterLink>
-            <RouterLink v-if="connected" to="/logout" class="navbar-link" :class="currentRouteName == 'logout' ? '' : 'active'">Déconnexion</RouterLink>
-            <RouterLink v-if="!connected" to="/login" class="navbar-link" :class="currentRouteName == 'login' ? '' : 'active'">Se connecter</RouterLink>
-            <RouterLink v-if="!connected" to="/register" class="navbar-link" :class="currentRouteName == 'register' ? '' : 'active'">S'inscrire</RouterLink>
+            <RouterLink v-if="connected" to="/logout" class="navbar-link" :class="currentRouteName == 'logout' ? 'active' : ''">Déconnexion</RouterLink>
+            <RouterLink v-if="!connected" to="/login" class="navbar-link" :class="currentRouteName == 'login' ? 'active' : ''">Se connecter</RouterLink>
+            <RouterLink v-if="!connected" to="/register" class="navbar-link" :class="currentRouteName == 'register' ? 'active' : ''">S'inscrire</RouterLink>
         </nav>
     </header>
 </template>
