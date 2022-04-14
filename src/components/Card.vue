51ef1c6e2e3b3448f76301bd4f1a487041fa1d29
chipseater <jeanpauldemalet@yahoo.fr>
2022-04-14 10:16:16 +0000

    formated date and added breakpoint variables

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,7 @@
     import data from "./../data.json"
 
     export default {
-        props: ["data"]
+        props: ["data"],
     }
 </script>
 
@@ -11,7 +11,13 @@
     <div class="card">
         <img :src="data.image" alt="placeholder" class="card-image">
         <h3 class="card-title">{{ data.title }}</h3>
-        <i>Niveau: {{ data.level }} • Ajouté le {{ data.publication_date }}</i>
+        <i>Niveau: {{ data.level }} • Ajouté le 
+        {{ 
+            (new Date(parseInt(data.publication_date))).getDate() + '/' +
+            (new Date(parseInt(data.publication_date))).getMonth() + '/' +
+            (new Date(parseInt(data.publication_date))).getFullYear()
+        }}
+        </i>
         <p class="card-descr">{{ data.description }}</p>
     </div>
 </template>
