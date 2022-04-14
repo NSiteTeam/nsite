71c6d651c334cdfeb6daed3a85b2b3430e010580
NeoPoete <neo.poete06@gmail.com>
2022-04-14 09:04:57 +0000

    [view/TimelineView.vue]  Finish timeline and history page

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -3,6 +3,7 @@
 
 <template>
     <div class="container">
+        <img class  ="img" src="https://www.gdiy.fr/wp-content/uploads/2019/09/Cedric_Villani-500x500.jpg" height="100"> 
         <ul>
             <li>
             <span></span>
@@ -10,6 +11,7 @@
                 <div class="title">les maths c vieux</div>
                 <div class="info">bref</div>
                 <div class="type">ZEJKAHFZJEEA</div>
+                <div class="img"></div>
             </div>
             <span class="number">
                 <span>CECI N4A AUCUN SENS</span>
@@ -37,100 +39,9 @@
             </div>
             <span class="number">
                 <span>SSS.f</span>
-                <span>17:45 (je meurt du cambridge</span>
+                <span>17:45 (je meurs du cambridge)</span>
             </span>
             </li>
         </ul>
     </div>
 </template>
-
-<style scoped>
-    @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700');
-    .bd {
-        height: 10vh;
-        font-family: 'Open Sans', sans-serif;
-        background: #86b7e7;
-        
-    }   
-    .container ul {
-        list-style: none;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        padding: 1px 100px;
-        color: #86b7e7;
-        font-size: 13px;
-    }
-    .container ul:before {
-        content: '';
-        width: 1px;
-        height: 100%;
-        position: absolute;
-        border-left: 2px dashed #86b7e7;
-  
-    }
-    .container ul li {
-        position: relative;
-        margin-left: 30px ;
-        background-color: rgba(255, 255, 255, 0.2);
-        padding: 14px;
-        border-radius: 6px;
-        width: 250px;
-        box-shadow: 0 0 4px rgba(0, 0, 0, .12), 0 2px 2px rgba(0, 0, 0, .08)  
-    }
-    .container ul li:not(:first-child) {
-        margin-top: 60px;
-        color:#ffffff;
-    }
-    .container ul li > span {
-        width: 2px;
-        height: 100%;
-        background: #86b7e7;
-        left: -30px;
-        top: 0;
-        position: absolute;
-    }
-    .container ul li > span:before, .container ul li > span:after {
-        content: '';
-        width: 8px;
-        height: 8px;
-        border-radius: 50%;
-        border: 2px solid #86b7e7;
-        position: absolute;
-        background: #86b7e7;
-        left: -5px;
-        top: 0;
-    }
-    .container ul li span:after {
-        top: 100%;
-    }
-    .container ul li > div {
-        margin-left: 10px;  
-    }   
-    .container div .title, .container div .type {
-        font-weight: 600;
-        font-size: 12px;
-    }
-    .container div .info {
-        font-weight: 300;
-    }
-    .container div > div {
-        margin-top: 5px;
-    }
-    .container span.number {
-        height: 100%;
-    }
-    .container span.number span {
-        position: absolute;
-        font-size: 10px;
-        left: -35px;
-        font-weight: bold;
-    }
-    .container span.number span:first-child {
-        top: 0;
-    }
-    .container span.number span:last-child {
-        top: 100%;
-    }
-</style>
