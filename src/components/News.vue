901f792fca205f15805c76ceb92a95951e9d5a0d
Silentium <raphael.delamaire@gmail.com>
2022-04-14 08:06:19 +0000

    [Home] News

--- src/components/News.vue
+++ src/components/News.vue
@@ -4,32 +4,9 @@
     <h1>Actualités</h1>
     <div class="liste-actualites">
       <ul>
-      <li>TESTONS LA LONGUEUR</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
-      <li>TEST</li>
+        <li>Le site est mintenant responsive sur PC</li>
+        <li>Les scrollbars sont maintenant custom</li>
+        
     </ul>
 
       
