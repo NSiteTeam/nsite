32faf21dc94e6d33986c157a508c22afbd49dee7
chipseater <74137062+chipseater@users.noreply.github.com>
2022-04-15 22:04:29 +0200

    [Repositories] Added date on detail

--- src/views/Repository.vue
+++ src/views/Repository.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
 import data from "./../data.json"
 
+const activeTab: String = "content"
+
 export default {
-    // @ts-ignore
-    data() {
+    data(): object {
         return {
             repoData: data.filter(repo => {
                 // @ts-ignore
@@ -11,18 +12,41 @@ export default {
             })[0]
         }
     },
-    date(): string {
-        // @ts-ignore
-        return (new Date(parseInt(data.publication_date))).getDate() + '/' +
-            (new Date(parseInt(data.publication_date))).getMonth() + '/' +
-            (new Date(parseInt(data.publication_date))).getFullYear()
+    methods: {
+        setActiveTab(tabId: String) {
+
+        }
+    },
+    computed: {
+        date(): string {
+            const timestamp = data.filter(repo => {
+                // @ts-ignore
+                return repo.id == parseInt(this.$route.params.id)
+            })[0].publication_date
+            return  (new Date(parseInt(timestamp))).getDate() + '/' +
+                    (new Date(parseInt(timestamp))).getMonth() + '/' +
+                    (new Date(parseInt(timestamp))).getFullYear()
+        }
     }
 }
 </script>
 
 <template>
-    {{ repoData }}
     <div id="repo-container">
-        
+        <h3 id="repo-title">{{ repoData.title }} <i>{{ date }}</i></h3>
+        <ul id="repo-menu">
+            <li id="content" class="active">
+                <span class="icon material-icons">
+                    cloud
+                </span>
+                Contenu
+            </li>
+            <li>
+                <span id="chat" class="icon material-icons">
+                    chat
+                </span>
+                Discussion
+            </li>
+        </ul>
     </div>
 </template>
