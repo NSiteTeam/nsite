b1ad6935a3c87bc882456dfc1ab72b308c0dcf8e
chipseater <74137062+chipseater@users.noreply.github.com>
2022-04-15 14:59:38 +0200

    levels are responsive in bigger screens

---
+++ src/views/Repository.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import data from "./../data.json"
+
+export default {
+    // @ts-ignore
+    data() {
+        return {
+            repoData: data.filter(repo => {
+                // @ts-ignore
+                return repo.id == parseInt(this.$route.params.id)
+            })[0]
+        }
+    },
+    date(): string {
+        // @ts-ignore
+        return (new Date(parseInt(data.publication_date))).getDate() + '/' +
+            (new Date(parseInt(data.publication_date))).getMonth() + '/' +
+            (new Date(parseInt(data.publication_date))).getFullYear()
+    }
+}
+</script>
+
+<template>
+    {{ repoData }}
+    <div id="repo-container">
+        
+    </div>
+</template>
