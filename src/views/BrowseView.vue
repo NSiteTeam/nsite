3e84ab93faf3049d9c65a4033098e5fc471e5395
chipseater <74137062+chipseater@users.noreply.github.com>
2022-04-19 07:41:17 +0200

    [Browse] trying to enhance browse

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -5,13 +5,18 @@ import data from "./../data.json"
 export default {
     data() {
         return {
-            reverse: 1
+            reverse: 1,
+            key: "date"
         }
     },
     methods: {
         changeOrder() {
             // @ts-ignore
             this.$data.reverse = this.$data.reverse * (-1)
+        },
+        changeKey(key: String) {
+            // @ts-ignore
+            this.$data.key = key
         }
     },
     components: {
@@ -23,7 +28,7 @@ export default {
             console.log(this.$data.reverse);
             return data.sort((a, b) => {
                 // @ts-ignore
-                return this.$data.reverse * (a.id - b.id)
+                return this.$data.reverse * (a[this.$data.key] - b[this.$data.key])
             })
         }
     }
@@ -33,7 +38,14 @@ export default {
 
 
 <template>
-    <button class="change-order-button" @click="changeOrder()">Changer l'ordre</button>
+    <div class="sort-container">
+        <button class="change-order-button" @click="changeOrder()">Changer l'ordre</button>
+        <ul class="sort-keys">
+            <li :@click="changeKey(key)" class="sort-key" v-for="key in Object.keys(output[0])" :key="key">
+                {{ key }}
+            </li>
+        </ul>
+    </div>
     <div id="browse-container">
         <Card :data=repo v-for="repo in output" :key="repo.id" />
     </div>
