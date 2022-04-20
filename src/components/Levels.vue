97cb671240eccdf79cd9796eae8e9f8b0aa73138
chipseater <74137062+chipseater@users.noreply.github.com>
2022-04-20 14:49:57 +0200

    [Home] Removed levels

--- src/components/Levels.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div id="levels">
-        <RouterLink to="/level/6" class="level"><span class="bold">6</span><sup>ème</sup></RouterLink>
-        <RouterLink to="/level/5" class="level"><span class="bold">5</span><sup>ème</sup></RouterLink>
-        <RouterLink to="/level/4" class="level"><span class="bold">4</span><sup>ème</sup></RouterLink>
-        <RouterLink to="/level/3" class="level"><span class="bold">3</span><sup>ème</sup></RouterLink>
-    </div>
-</template>
