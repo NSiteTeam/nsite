6a201b465d64f8bbdfba5d20112f88d5fd4714d5
chipseater <74137062+chipseater@users.noreply.github.com>
2022-04-20 14:35:20 +0200

    [Browse] enhanced sorting system

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -7,10 +7,11 @@ export default {
         return {
             reverse: -1,
             key: "publication_date",
+            selected_level: -1,
             sort_keys: {
-                publication_date: "date",
-                title: "titre",
-                level: "niveau"
+                publication_date: "Par date",
+                title: "Alphabétique",
+                level: "Par niveau"
             }
         }
     },
@@ -22,6 +23,10 @@ export default {
         changeKey(key: String) {
             // @ts-ignore
             this.$data.key = key
+        },
+        selectLevel(level: number) {
+            // @ts-ignore
+            this.$data.selected_level = level
         }
     },
     components: {
@@ -36,9 +41,31 @@ export default {
                     return this.$data.reverse * a.title.localeCompare(b.title)
                 })
             }
-            return data.sort((a, b) => {
-                // @ts-ignore
-                return this.$data.reverse * (a[this.$data.key] - b[this.$data.key])
+            // @ts-ignore
+            if (this.$data.selected_level == -1){
+                return data.sort((a, b) => {
+                    // @ts-ignore
+                    return this.$data.reverse * (a[this.$data.key] - b[this.$data.key])
+                })
+            } else {
+                return data.sort((a, b) => {
+                    // @ts-ignore
+                    return this.$data.reverse * (a[this.$data.key] - b[this.$data.key])
+                }).filter(repo => {
+                    // @ts-ignore
+                    return repo.level == this.$data.selected_level
+                })
+            }
+        },
+        levels(): Array<number> {
+            const levels: Array<number> = []
+            data.forEach(repo => {
+                if(!(levels.includes(repo.level))) {
+                    levels.push(repo.level)
+                }
+            })
+            return levels.sort((a, b) => {
+                return b-a
             })
         }
     }
@@ -48,26 +75,33 @@ export default {
 
 
 <template>
-    <div class="sort-container">
-        <ul class="sort-keys">
-            <button class="change-order-button" @click="changeOrder()">
-                <span v-if="reverse == 1" class="material-icons">
-                    arrow_drop_up
-                </span>
-                <span v-else class="material-icons">
-                    arrow_drop_down
-                </span>
-                Changer l'ordre
-            </button>
-            <li @click="changeKey(sortKey[0])" 
-            class="sort-key" :class="key == sortKey[0] ? 'active' : ''" 
-            v-for="sortKey in Object.entries(sort_keys)" 
-            :key="sortKey">
-                <span :class="key == sortKey[0] ? 'selector' : ''">{{ key == sortKey[0] ? '•' : '' }}</span>
-                {{ sortKey[1] }}
-            </li>
-        </ul>
-    </div>
+    <h2>Type de tri :</h2>
+    <ul class="sort-keys">
+        <button class="change-order-button" @click="changeOrder()">
+            <span v-if="reverse == 1" class="material-icons">
+                arrow_drop_up
+            </span>
+            <span v-else class="material-icons">
+                arrow_drop_down
+            </span>
+            Changer l'ordre
+        </button>
+        <li @click="changeKey(sortKey[0])" 
+        :class="key == sortKey[0] ? 'active' : ''" 
+        v-for="sortKey in Object.entries(sort_keys)" 
+        :key="sortKey">
+            {{ sortKey[1] }}
+        </li>
+        <li :class="-1 == selected_level ? 'active' : ''" 
+        class="level-button-all" @click="selectLevel(-1)">
+            Tout
+        </li>
+        <li :class="level == selected_level ? 'active' : ''" 
+        class="level-button" @click="selectLevel(level)" v-for="level in levels" :key="level">
+            {{ level }}<sup>ème</sup>
+        </li>
+    </ul>
+    <h2>Résultats :</h2>
     <div id="browse-container">
         <Card :data=repo v-for="repo in output" :key="repo.id" />
     </div>
