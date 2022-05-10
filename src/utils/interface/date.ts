85319dbb10688552c82b0c27f0aba635a6652e6c
chipseater <jeanpauldemalet@yahoo.fr>
2022-05-10 09:25:55 +0000

    [Utils] added CustomDate class

---
+++ src/utils/interface/date.ts
@@ -0,0 +1,14 @@
+export default interface date {
+    // The attributes of the date
+    seconds: number
+    minutes: number
+    hours: number
+    day: number
+    month: number
+    year: number
+    digits: Array<number>
+
+    isGreaterThan(otherDate: date): boolean
+    isLessThan(otherDate: date): boolean
+    isGreaterThan(otherDate: date): boolean
+}
