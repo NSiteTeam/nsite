754809e1bd4d4774c7001ca108cd3d8ef85e32d9
Silentium <raphael.delamaire@gmail.com>
2022-05-19 10:13:35 +0000

    [Dashboard] add documents v-0.1

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,24 +1,7 @@
 <template>
-    <div id="profile-container"  class="shadow2">
-        salut les tchoupis fans
+    <div id="dashboard-container"  class="shadow2">
+        Ajouter des documents
+        <button class="add-documents"><p>+</p></button>
 
     </div>
 </template>
-
-<script setup lang="ts">
-    import { databaseClient } from '@/database/implementation';
-    import { ref } from '@vue/reactivity';
-    import type { Ref } from 'vue';
-    import type { Username } from '@/database/interface/username';
-    databaseClient
-
-    console.log(databaseClient)
-
-    // TODO: Display all usernames in the database
-    const email = databaseClient.email
-    const val_email = email.value
-
-    const usernames: Ref<Array<Username>> = ref([])
-
-    databaseClient.getUsername()
-</script>
