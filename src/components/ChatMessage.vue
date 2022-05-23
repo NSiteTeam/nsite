3bbd729a8f10b03cbae3770d9ddce74753a5857f
chipseater <74137062+chipseater@users.noreply.github.com>
2022-05-23 21:31:04 +0200

    [Chat] trying to make a refactor for chat messages

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
-    import { databaseClient } from "@/database/implementation"
+import { databaseClient } from "@/database/implementation"
+import CustomDate from "@/utils/classes/CustomDate"
+import { Ref, ref } from "vue";
 
-    const { message } = defineProps(['message'])
+const { message } = defineProps(['message'])
+const editMode: Ref<boolean> = ref(false)
+
+function formatDate(ISOdate: string) {
+    return CustomDate.ISOStringToCustomDate(ISOdate).beautify()
+}
+
+function changeEditMode() {
+    editMode.value = !editMode.value
+}
 </script>
 
 <template>
     <div></div>
-    <!-- <div class="message">
-        <p class="author">
+    <div class="message">
+        <!-- <p class="author">
             {{ uuidToUsername(message.author) }}
-        </p>
+        </p> -->
         <p class="date">{{ formatDate(message.date) }}</p>
-        <input class="message-edition-input" v-if="getEditModeByMessageId(message.id)"
-        v-model="message.content" type="text" :key="index">
-        <p class="content" v-else>{{ message.content }}</p>
+        <!-- <input class="message-edition-input" v-if="getEditModeByMessageId(message.id)"
+        v-model="message.content" type="text" :key="index"> -->
+        <p class="content">{{ message.content }}</p>
         <div v-if="message.author == databaseClient.uuid.value || message.author == null" 
         class="messageButtons">
-            <button @click="setEditMode(message.id, !getEditModeByMessageId(message.id), 
-            message.content)" class="message-button">
-                {{ getEditModeByMessageId(message.id) ? "Valider" : "Modifier" }}
+            <button @click="changeEditMode()" class="message-button">
+                {{ editMode ? "Valider" : "Modifier" }}
             </button>
             <button @click="databaseClient.deleteMessage(message.id)"
             class="message-button">Supprimer</button>
         </div>
         {{ message }}
-    </div> -->
+    </div>
 </template>
