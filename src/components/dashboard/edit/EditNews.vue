94df386900a31164ec83259d028651b144ac04f9
chipseater <jeanpauldemalet@yahoo.fr>
2022-06-02 10:17:16 +0000

    [News] added add button

--- src/components/dashboard/edit/EditNews.vue
+++ src/components/dashboard/edit/EditNews.vue
@@ -7,9 +7,14 @@ import type { Ref } from "vue";
 import { ref } from "vue";
 
 const newsPayload: Ref<News[] | null> = ref([])
+const toggleNewsAdd: Ref<boolean> = ref(false)
 console.log(databaseClient.fetchedNews.value.length == 0)
 if (databaseClient.fetchedNews.value.length == 0) {
     await databaseClient.fetchNews(10)
+}
+
+function toggleAdd() {
+    toggleNewsAdd.value = !toggleNewsAdd.value
 }
 
 newsPayload.value = databaseClient.fetchedNews.value
@@ -18,6 +23,10 @@ newsPayload.value = databaseClient.fetchedNews.value
 <template>
     <h3>Modifier les actualités</h3>
     <div class="news-editor">
+        <div class="news-editor-add">
+            <span class="material-icons" @click="toggleAdd()">{{ toggleNewsAdd ? 'done' : 'add' }}</span>
+            Ajouter une actualité
+        </div>
         <NewsItem :news="news" v-for="(news, index) in databaseClient.fetchedNews.value" 
         :key="index" class="news-editor-item" />
     </div>
