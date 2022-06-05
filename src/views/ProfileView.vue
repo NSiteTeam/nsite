1f90c9fcebd263e344fa1655472d41d479652069
chipseater <74137062+chipseater@users.noreply.github.com>
2022-06-05 21:05:20 +0200

    [dashboard] email hidden with a button

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -9,11 +9,14 @@
             </span>
             <div class="user-details">
                 <div class="label">Adresse mail: </div>
-                <div class="value">{{ email }}</div>
+                <div class="value">
+                    {{ email }}
+                    <button @click="toggleEmail()">
+                        {{ displayEmail ? 'Masquer' : 'Afficher' }}
+                    </button>
+                </div>
                 <div class="label">Nom d'utilisateur: </div>
                 <div class="value">{{ username }}</div>
-                <div class="label">Uuid : </div>
-                <div class="value">{{ uuid }}</div>
                 <div class="label">Rôles : </div>
                 <ul class="value">
                     <li class="permission" v-for="(permission, index) in permissions" :key="index">
@@ -25,12 +28,29 @@
     </div>
     <RouterLink  to="/dashboard" class="navbar-link">Dashboard</RouterLink>
 </template>
-<!--TODO: REWORK COMPLETLY THIS HORRIBLE THING (: -->
+
 <script setup lang="ts">
     import { databaseClient } from '@/database/implementation';
+    import { computed, ref } from 'vue';
 
-    const email = databaseClient.user.value?.email
-    const uuid = databaseClient.user.value?.uuid
     const username = databaseClient.user.value?.username
+    const email = computed(
+        _ => {
+            if (databaseClient.user.value?.email && !displayEmail.value) {
+                return (databaseClient.user.value.email
+                .split('@')[0].replace(/\w/g,'*') + '@'
+                + databaseClient.user.value.email
+                .split('@')[1])
+            } else {
+                return databaseClient.user.value?.email
+            }
+        }
+    )
     const permissions = databaseClient.user.value?.permissions
+    console.log(databaseClient.user.value)
+    const displayEmail = ref(false)
+
+    function toggleEmail() {
+        displayEmail.value = !displayEmail.value
+    }
 </script>
