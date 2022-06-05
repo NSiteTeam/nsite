cd946f91c2716623f45d21731038e4836c1e37e3
chipseater <74137062+chipseater@users.noreply.github.com>
2022-06-05 18:55:22 +0200

    [dashboard] reworked profileView

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,21 +1,29 @@
 <template>
     <div id="profile-container"  class="shadow2">
-        <span class="profile-title">
-            <span class="material-icons white">
-                account_circle
+        <div class="user-infos">
+            <span class="profile-title">
+                <span class="material-icons white">
+                    account_circle
+                </span>
+                {{ username }}
             </span>
-            Mon profil:
-        </span>
-        <ul class="table-part">
-            <li class="white">Adresse mail: <i>{{ email }}</i></li>
-            <li class="white">Nom d'utilisateur: <i>{{ username }}</i></li>
-            <li class="white">Uuid : <i>{{ uuid }}</i></li>
-            <li class = "white">Rôle : <i> {{ permissions }}</i></li>
-        </ul>
-
-        <RouterLink  to="/dashboard" class="navbar-link">Dashboard</RouterLink>
-
+            <div class="user-details">
+                <div class="label">Adresse mail: </div>
+                <div class="value">{{ email }}</div>
+                <div class="label">Nom d'utilisateur: </div>
+                <div class="value">{{ username }}</div>
+                <div class="label">Uuid : </div>
+                <div class="value">{{ uuid }}</div>
+                <div class="label">Rôles : </div>
+                <ul class="value">
+                    <li class="permission" v-for="(permission, index) in permissions" :key="index">
+                        {{ permission }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
+    <RouterLink  to="/dashboard" class="navbar-link">Dashboard</RouterLink>
 </template>
 <!--TODO: REWORK COMPLETLY THIS HORRIBLE THING (: -->
 <script setup lang="ts">
@@ -24,5 +32,5 @@
     const email = databaseClient.user.value?.email
     const uuid = databaseClient.user.value?.uuid
     const username = databaseClient.user.value?.username
-    const permissions = databaseClient.user.value?.permissions.join(', ')
+    const permissions = databaseClient.user.value?.permissions
 </script>
