2fd423899267deceffe2041c86cb0d58982b9524
chipseater <74137062+chipseater@users.noreply.github.com>
2022-06-06 21:12:14 +0200

    [Dashboard/deposits] file upload operationnal

--- src/components/dashboaard/ManageDeposits.vue
+++ src/components/dashboaard/ManageDeposits.vue
@@ -1,20 +1,28 @@
 <script setup lang="ts">
+    // @ts-ignore
+    import FileItem from "@/components/dashboard/edit/items/FileItem.vue"
     import { databaseClient } from "@/database/implementation";
     import type { Repository } from "@/database/interface/repositories";
-    import FileItem from "@/components/dashboard/edit/items/FileItem.vue"
-    import { watch, ref, toRaw, onMounted } from "vue";
-    import type { Ref } from "vue";
     import { Level } from '@/database/interface/level'
+    import { watch, ref, toRaw, onMounted, computed} from "vue";
+    import type { Ref, ComputedRef  } from "vue";
 
+    const displaySidePannelNewDepo: Ref<boolean> = ref(false)
+    const displaySidePannelNewFile: Ref<boolean> = ref(false)
     const newDepoLevel: Ref<number | null> = ref(null)
-    const displaySidePannel: Ref<boolean> = ref(false)
     const newDepoDescription: Ref<string> = ref("")
+    const error: Ref<string | null> = ref(null)
+    const success: Ref<boolean> = ref(false)
+    const loading: Ref<boolean> = ref(false)
+    const errorFiles: Ref<string | null> = ref(null)
+    const successFiles: Ref<boolean> = ref(false)
+    const loadingFiles: Ref<boolean> = ref(false)
     const newDepoTitle: Ref<string> = ref("")
     const depositTableIsExpanded = ref(true)
-    const success: Ref<boolean> = ref(false)
-    const error: Ref<string | null> = ref(null)
-    const loading: Ref<boolean> = ref(false)
     const files: Ref<any[]> = ref([])
+    const rawFile: Ref<any> = ref()
+    const newFile: Ref<string> = ref("")
+    const newFileMessage = ref("")
     const selectedDeposit = ref()
     const deposits = ref()
 
@@ -33,8 +41,15 @@
         })
     }
 
-    function toggleSidePannel() {
-        displaySidePannel.value = !displaySidePannel.value
+    function toggleSidePannel(target: string) {
+        if (target == 'depo') {
+            displaySidePannelNewDepo.value = true
+        } else if (target == 'file') {
+            displaySidePannelNewFile.value = true
+        } else {
+            displaySidePannelNewDepo.value = false
+            displaySidePannelNewFile.value = false
+        }
     }
 
     function fetchFiles() {
@@ -71,6 +86,28 @@
         setTimeout(_ => [success.value, loading.value, error.value] = [false, false, null], 3000)
     }
 
+    const uploadInfo: ComputedRef<string> = computed(() => {
+      return files.value.length == 1
+        ? files.value[0].name : ''
+    })
+
+    function watchFiles(e: any) {
+        files.value = Array.from(e.target.files) || []
+        rawFile.value = e.target.files[0] || []
+        newFile.value = e.target.files[0].name
+    }
+
+    async function uploadFile() {
+        loadingFiles.value = true
+        await databaseClient.uploadFileToDeposit(
+            rawFile.value, selectedDeposit.value.title, 
+            newFileMessage.value, newFile.value
+        ).then(_ => successFiles.value = true)
+        .catch(res => errorFiles.value = res)
+        loadingFiles.value = false
+        setTimeout(_ => [successFiles.value, errorFiles.value] = [false, null], 3000)
+    }
+
     onMounted(fetchFiles)
     watch(selectedDeposit, fetchFiles)
     watch(success, fetchFiles)
@@ -81,6 +118,9 @@
     <div class="good" v-if="success">Le dépôt a bien été créé</div>
     <div class="indication" v-else-if="loading">Création en cours ...</div>
     <div class="error" v-else-if="error">{{ error }}</div>
+    <div class="good" v-if="successFiles">Le fichier a bien été envoyé</div>
+    <div class="indication" v-else-if="loadingFiles">Envoi en cours ...</div>
+    <div class="error" v-else-if="errorFiles">{{ errorFiles }}</div>
     <div id="manage-deposit">
         <div id="deposit-list" :class="{ 'hidden': !depositTableIsExpanded }">
             <h3 id="deposit-list-title">Vos depôts de ressources</h3>
@@ -102,7 +142,8 @@
                 >
                     {{ deposit.title }}
                 </li>
-                <li @click="toggleSidePannel()" class='deposit-list-element' id="add-deposit-button">
+                <li @click="toggleSidePannel('depo')" 
+                class='deposit-list-element' id="add-deposit-button">
                     <span class="material-icons white">
                         add
                     </span>
@@ -114,27 +155,46 @@
         </div>
     </div>
     <div class="files">
+        <div class="files-actions">
+            <button class="add files-action" @click="toggleSidePannel('file')">
+                <span class="material-icons plus">add</span>
+                <span class="label">Ajouter un fichier</span>
+                
+            </button>
+            <button class="edit files-action">
+                <span class="material-icons pen">edit</span>
+                <span class="label">Modifier le dépôt</span>
+            </button>
+            <button class="delete files-action">
+                <span class="material-icons bin">delete_forever</span>
+                <span class="label">Supprimer le dépôt</span>
+            </button>
+        </div>
         <FileItem v-for="(file, index) in files" :key="index" :file="file" />
     </div>
-    <div class="mask" v-if="displaySidePannel" @click="toggleSidePannel()"></div>
-    <div class="side-pannel" v-if="displaySidePannel">
-        <h4 class="side-pannel-title">
-            <div class="material-icons side-pannel-title-cross" @click="toggleSidePannel()">close</div>
+    <div class="mask" v-if="displaySidePannelNewDepo || displaySidePannelNewFile" 
+    @click="toggleSidePannel()"></div>
+    <div class="side-pannel-new-depo" v-if="displaySidePannelNewDepo">
+        <h4 class="side-pannel-new-depo-title">
+            <div class="material-icons side-pannel-new-depo-title-cross" @click="toggleSidePannel()">
+                close
+            </div>
             Créez un nouveau dépot de ressources
         </h4>
-        <div class="side-pannel-fields">
-            <div class="side-pannel-field">
-                <label class="side-pannel-field-label" for="depo-name">Nom du dépôt</label>
-                <input v-model="newDepoTitle" class="side-pannel-field-input" type="text" name="depo-name" id="depo-name" />
-            </div>
-            <div class="side-pannel-field">
-                <label class="side-pannel-field-label" for="depo-name">Description</label>
-                <input v-model="newDepoDescription" class="side-pannel-field-input" placeholder="Optionnel"
+        <div class="side-pannel-new-depo-fields">
+            <div class="side-pannel-new-depo-field">
+                <label class="side-pannel-new-depo-field-label" for="depo-name">Nom du dépôt</label>
+                <input v-model="newDepoTitle" class="side-pannel-new-depo-field-input" 
                 type="text" name="depo-name" id="depo-name" />
             </div>
-            <div class="side-pannel-field">
-                <label class="side-pannel-field-label" for="depo-name">Niveau</label>
-                <select v-model="newDepoLevel" class="side-pannel-field-input">
+            <div class="side-pannel-new-depo-field">
+                <label class="side-pannel-new-depo-field-label" for="depo-name">Description</label>
+                <input v-model="newDepoDescription" class="side-pannel-new-depo-field-input"
+                type="text" name="depo-name" id="depo-name" ref="newFilemessage" placeholder="Optionnel" />
+            </div>
+            <div class="side-pannel-new-depo-field">
+                <label class="side-pannel-new-depo-field-label" for="depo-name">Niveau</label>
+                <select v-model="newDepoLevel" class="side-pannel-new-depo-field-input">
                     <option selected value="" disabled>-- Sélectionnez un niveau --</option>
                     <option :value="index" v-for="(level, index) in levels"
                     :key="level.index" id="level" placeholder="Niveau">
@@ -143,18 +203,62 @@
                 </select>
             </div>
         </div>
-        <div class="side-pannel-files">
-            <div class="side-pannel-files-header">
-                <h4 class="side-pannel-files-header-title">Ajoutez des Fichiers</h4>
-                <h5 class="side-pannel-files-header-subtitle">Inaugurez votre dépôt en y ajoutant du contenu</h5>
+        <div class="side-pannel-new-depo-files">
+            <div class="side-pannel-new-depo-files-header">
+                <h4 class="side-pannel-new-depo-files-header-title">Ajoutez des Fichiers</h4>
+                <h5 class="side-pannel-new-depo-files-header-subtitle">
+                    Inaugurez votre dépôt en y ajoutant du contenu
+                    </h5>
             </div>
-            <div class="side-pannel-files-menu">
-                <i>Ceci est un texte générique à remplacer par le menu pour ajouter un fichier (en construction)</i>
+            <div class="side-pannel-new-depo-files-menu">
+                <i>
+                    Ceci est un texte générique à remplacer 
+                    par le menu pour ajouter un fichier (en construction)
+                </i>
             </div>
         </div>
-        <div class="side-pannel-bottom-buttons">
-            <button class="side-pannel-bottom-buttons-cancel" @click="toggleSidePannel()">Annuler</button>
-            <button class="side-pannel-bottom-buttons-submit" @click="addDeposit()">Envoyer</button>
+        <div class="side-pannel-new-depo-bottom-buttons">
+            <button class="side-pannel-new-depo-bottom-buttons-cancel" @click="toggleSidePannel()">
+                Annuler
+            </button>
+            <button class="side-pannel-new-depo-bottom-buttons-submit" @click="addDeposit()">
+                Envoyer
+            </button>
+        </div>
+    </div>
+    <div class="side-pannel-new-file" v-if="displaySidePannelNewFile">
+        <h4 class="side-pannel-new-file-title">
+            <div class="material-icons side-pannel-new-file-title-cross" 
+            @click="toggleSidePannel()">
+                close
+            </div>
+            Ajoutez un nouveau fichier dans&nbsp;<u>{{ selectedDeposit.title }}</u>
+            
+        </h4>
+        <div class="side-pannel-new-file-fields">
+            <div>
+                <div class="side-pannel-new-file-field-label">Votre fichier</div>
+                <label class="side-pannel-new-file-field upload-file" for="file-input">
+                    Ajouter un fichier
+                    <input class="side-pannel-new-file-field-input-text" type="text" 
+                    ref="newFileMessageElement" v-model="newFile" />
+                    <input class="side-pannel-new-file-field-file-input" @change="watchFiles"
+                    placeholder="Optionnel" type="file" name="depo-name" id="file-input" />
+                </label>
+            </div>
+            <div class="side-pannel-new-file-field">
+                <label class="side-pannel-new-file-field-label" for="depo-name">Description</label>
+                <input v-model="newFileMessage" class="side-pannel-new-file-field-input" placeholder="Optionnel"
+                type="text" name="depo-name" id="file-name" />
+            </div>
+        </div>
+        <div class="side-pannel-new-file-bottom-buttons">
+            <button class="side-pannel-new-file-bottom-buttons-cancel" @click="toggleSidePannel()">
+                Annuler
+            </button>
+            <button class="side-pannel-new-file-bottom-buttons-submit" @click="uploadFile()">
+                Envoyer
+            </button>
         </div>
     </div>
 </template>
--- src/database/supabase/supabase.ts
+++ src/database/supabase/supabase.ts
@@ -16,7 +16,6 @@ import type Message from '../interface/message'
 import SupabaseMessage from './supabase_message'
 import { SupabaseUser } from './supabase_user'
 import { SupabasePermissionHelper } from './supabase_permission_helper'
-import { SupabaseLevelHelper } from './supabase_level_helper'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
@@ -111,18 +110,17 @@ export class SupabaseClient implements DatabaseClient {
      * @param password the password of the user
      * @returns if the account was created or not. The return can be true even if the email is not yet verified
      */
-    async logout(): Promise<boolean> {
+    async logout(): Promise<any> {
         console.log("Trying to sign out")
 
         const { error } = await supabase.auth.signOut()
-
-        if (error) {
-            console.log("Error during disconnection", error)
-        }
-
-        this.updateUserInfos()
-
-        return error == null
+        return new Promise((resolve, reject) => {
+            if (!error) {
+                resolve("Vous êtes déconnecté")
+            } else {
+                reject(error)
+            }
+        })
     }
 
     /**
@@ -272,34 +270,32 @@ export class SupabaseClient implements DatabaseClient {
     async getRepos(id?: number): Promise<Repository[]> {
         console.log("Trying to fetch deposits in the database")
 
-        try {
-            // Here we can directly manipulate the database as deposits are public
-            let { data, error } = !id ? await supabase.from('deposits').select() :
-            await supabase.from('deposits').select().eq("id", id).maybeSingle()
-            
-            if (id) {
-                data = [data]
-            }
-
-            if (error) {
-                throw error
-            }
-
-            if (!data) {
-                throw "Data returned by the request is null"
-            }
-
-            return data.map((repository: any) => new SupabaseRepository(
-                repository['id'],
-                repository['title'],
-                SupabaseLevelHelper.getLevelById(repository['level']),
-                repository['publication_date'],
-                repository['description'],
-                repository['content'],
-            ))
-        } catch (error) {
-            console.log('Error while fetching deposits', error)
+        // Here we can directly manipulate the database as deposits are public
+        let { data, error } = !id ? await supabase.from('deposits').select() :
+        await supabase.from('deposits').select().eq("id", id).maybeSingle()
+        
+        if (id) {
+            data = [data]
         }
+
+        return new Promise((resolve, reject) => {
+            if (error == null && data) {
+                this.repositories.value.push(data[0])
+                resolve(data.map((repositories: Repository) => {
+                    return new SupabaseRepository(
+                        repositories['id'],
+                        repositories['title'],
+                        repositories['level'],
+                        repositories['publication_date'],
+                        repositories['description'],
+                        repositories['content'],
+                    )
+                }))
+            } else if (error) {
+                reject(`Error while fetching deposits, 
+                probably caused by changes in the database: ` + error.message)
+            }
+        })
     }
 
     async getOwnedDeposits(): Promise<Repository[]> {
@@ -549,15 +545,23 @@ export class SupabaseClient implements DatabaseClient {
         2: registers the file object in the dB
         3: selects the already present files in the depo
         4: updates the content in the depo */
-    async uploadFileToDeposit(file: File, deposit: string, message: string): Promise<string> {
-        // Removes the diacritics from the file name
+    async uploadFileToDeposit(file: File, deposit: string, message: string, fileName?: string): Promise<string> {
+        // Removes the bad caracters from the file name
+        const newName = (fileName || file.name)
+        .replace(/[<>{}%`\[\]~#^:'’"/\\|?*]/g, ' ')
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+        // Removes the bad caracters from the deposit name
+        const cleanDepositName = deposit
+        .replace(/[<>{}%`\[\]~#^:'’"/\\|?*]/g, ' ')
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
         const author = this.uuid.value ? await this.getUsername(this.uuid.value) : "anonyme"
         // Uploads data to the storage bucket
-        const escapedFile = new File([file], file.name
-            .normalize("NFD").replace(/[\u0300-\u036f]/g, ""), {type: file.type});
-        console.log(escapedFile.name)
+        const escapedFile = new File([file], newName, {type: file.type});
+        
+        console.log(deposit + '/' + newName)
         const { data, error } = await supabase.storage.from('depositsfiles').upload(
-            escapedFile.name, escapedFile, {
+            cleanDepositName + '/' + newName, escapedFile, {
                 cacheControl: '3600',
                 upsert: false
             }
@@ -583,10 +587,11 @@ export class SupabaseClient implements DatabaseClient {
                 // @ts-ignore res.data vaut any donc il est pas content qu'on lise des propriétés dessus
                 res.data ? console.log(responseForTheSelect.data) : null
 
+                const oldData = responseForTheSelect.data.content || []
                 // Adds the file to the depo
                 const responseForTheUpdate = await supabase.from("deposits")
                 // @ts-ignore res.data is any so he is not happy
-                .update([{ "content": responseForTheSelect.data.content.concat(res.data[0].id) }])
+                .update([{ "content": oldData.concat(res.data[0].id) }])
                 .match({ "title": deposit })
 
                 res.error ? console.warn(responseForTheUpdate.error) : null
