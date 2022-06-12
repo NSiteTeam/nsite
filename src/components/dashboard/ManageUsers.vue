fb1e3324a68400fbea6de7ba3e023756055d58e2
chipseater <74137062+chipseater@users.noreply.github.com>
2022-06-12 10:38:54 +0200

    [dashboard/manageUsers] added sort buttons

--- src/components/dashboard/ManageUsers.vue
+++ src/components/dashboard/ManageUsers.vue
@@ -1,53 +1,157 @@
 <script setup lang="ts">
-    // @ts-ignore
-    import { SupabasePermissionHelper } from '@/database/supabase/supabase_permission_helper'
-    import DataColumn from '@/components/dashboard/DataColumn.vue'
-    import { Permission } from "@/database/interface/permissions"
-    import { databaseClient } from "@/database/implementation"
-    import type { User } from '@/database/interface/user'
-    import type { Ref } from 'vue'
-    import { ref } from 'vue'
+// @ts-ignore
+import { SupabasePermissionHelper } from '@/database/supabase/supabase_permission_helper'
+import { Permission } from '@/database/interface/permissions'
+import { databaseClient } from '@/database/implementation'
+import type { User } from '@/database/interface/user'
+import type { Ref } from 'vue'
+import { computed, ref } from 'vue'
 
-    const users: Ref<any[]> = ref([])
-    const selectedUser: Ref<User | undefined> = ref()
+enum sortTypes {
+  USERNAME,
+  UUID,
+  HIGHEST_ROLE,
+}
 
-    await databaseClient.getAllUsers()
-    .then(res => {
-        users.value = res
-    }).catch(err => {
-        console.warn(err)
-    })
+enum orders {
+  DESCENDING = -1,
+  ASCENDING = 1,
+}
 
-    function getPermissionFromId(permissionId: number) {
-        return SupabasePermissionHelper.permissionFromId(permissionId)
-    }
+enum sortIcons {
+  DESCENDING = 'expand_more',
+  ASCENDING = 'expand_less',
+}
+
+const users: Ref<any[]> = ref([])
+const sortOrder: Ref<orders> = ref(orders.DESCENDING)
+// const selectedUser: Ref<User | undefined> = ref()
+const selectedSortType: Ref<sortTypes> = ref(sortTypes.HIGHEST_ROLE)
+
+await databaseClient
+  .getAllUsers()
+  .then((res) => {
+    users.value = res
+  })
+  .catch((err) => {
+    console.warn(err)
+  })
+
+const sortedData = computed(() => {
+  switch (selectedSortType.value) {
+    case sortTypes.HIGHEST_ROLE:
+      return users.value.sort((a, b) => {
+        return (
+          sortOrder.value *
+          (Math.max(...b.roles.map((role: any) => Number(role))) -
+            Math.max(...a.roles.map((role: any) => Number(role))))
+        )
+      })
+    case sortTypes.UUID:
+      return users.value.sort((a, b) => {
+        return sortOrder.value * b.user.localeCompare(a.user)
+      })
+    case sortTypes.USERNAME:
+      return users.value.sort((a, b) => {
+        return sortOrder.value * b.username.localeCompare(a.username)
+      })
+  }
+})
+
+function reverseSort() {
+  sortOrder.value *= -1
+}
+
+function changeSortType(newSortType: sortTypes) {
+  if (selectedSortType.value == newSortType) {
+    reverseSort()
+  } else {
+    selectedSortType.value = newSortType
+  }
+}
+
+function getCorrespondingIcon(sortType: sortTypes) {
+  if (
+    orders.ASCENDING == sortOrder.value &&
+    sortType == selectedSortType.value
+  ) {
+    return sortIcons.ASCENDING
+  } else if (
+    orders.DESCENDING == sortOrder.value &&
+    sortType == selectedSortType.value
+  ) {
+    return sortIcons.DESCENDING
+  } else {
+    return ''
+  }
+}
+
+function getPermissionFromId(permissionId: number): string {
+  return SupabasePermissionHelper.permissionFromId(permissionId)
+}
 </script>
 
-
 <template>
-    <div class="manage-users">
-        <div class="manage-users-table">
-            <div class="header">
-                <div class="cell">Nom d'utilisateur</div>
-                <div class="cell">Identifiant unique</div>
-                <div class="cell">Rôle le plus haut</div>
+  <div class="manage-users">
+    <div class="manage-users-table">
+      <div class="header">
+        <div
+          class="cell title-cell"
+          @click="changeSortType(sortTypes.USERNAME)"
+        >
+          <div class="cell-content">
+            <div class="material-icons">
+              {{
+                getCorrespondingIcon(sortTypes.USERNAME)
+              }}
             </div>
-            <div class="row" v-for="(user, index) in users" :key="index">
-                <div class="cell">
-                    {{ user.username }}
-                </div>
-                <div class="cell">
-                    {{ user.user }}
-                </div>
-                <div class="cell">
-                    {{ getPermissionFromId(Math.max(... user.roles.map(role => Number(role)))) }}
-                </div>
-            </div>
-            <div class="footer">
-                <div class="cell"></div>
-                <div class="cell">Afficher plus</div>
-                <div class="cell"></div>
-            </div>
+            Nom d'utilisateur
+          </div>
         </div>
+        <div class="cell title-cell" @click="changeSortType(sortTypes.UUID)">
+          <div class="cell-content">
+            <div class="material-icons">
+              {{
+                getCorrespondingIcon(sortTypes.UUID)
+              }}
+            </div>
+            Identifiant unique
+          </div>
+        </div>
+        <div
+          class="cell title-cell"
+          @click="changeSortType(sortTypes.HIGHEST_ROLE)"
+        >
+          <div class="cell-content">
+            <div class="material-icons">
+              {{
+                getCorrespondingIcon(sortTypes.HIGHEST_ROLE)
+              }}
+            </div>
+            Rôle le plus haut
+          </div>
+        </div>
+      </div>
+      <div class="row" v-for="(user, index) in sortedData" :key="index">
+        <div class="cell">
+          {{ user.username }}
+        </div>
+        <div class="cell">
+          {{ user.user }}
+        </div>
+        <div class="cell">
+          {{
+            getPermissionFromId(
+              Math.max(...user.roles.map((role) => Number(role))),
+            )
+          }}
+        </div>
+      </div>
+      <div class="footer">
+        <div class="cell"></div>
+        <div class="cell">Afficher plus</div>
+        <div class="cell"></div>
+      </div>
     </div>
+  </div>
 </template>
