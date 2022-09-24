66dec7fff0081e2d21ce33d2175e301a13696693
NeoPoete <neo.poete06@gmail.com>
2022-09-24 17:08:51 +0000

    [ContactUs.vue] reworked on the page. Please dont touch it yet

--- src/components/contact/ContactUs.vue
+++ src/components/contact/ContactUs.vue
@@ -3,11 +3,9 @@
     <form @submit.prevent="tryRegister" class="h-full w-full">
       <div class="flex h-full flex-col">
         <LargeTitle>Nous contacter</LargeTitle>
-
         <p class="text-sm italic text-gray-500">
           Nous tâcherons de répondre au plus vite!
         </p>
-
         <InputField
           class="mt-4"
           type="text"
@@ -16,7 +14,6 @@
           v-model="name"
           :error="texterror"
         />
-
         <InputField
           class="mt-4"
           type="email"
@@ -25,7 +22,6 @@
           :error="emailError"
           placeholder="exemple.test@votremail.com"
         />
-
         <TextArea
           class="block h-64 w-full"
           label="Votre message"
@@ -33,17 +29,25 @@
           id="message"
           v-model="message"
         />
-
         <SubmitButton
           message="Envoyer"
           :invalidFields="invalidFields"
           :submitting="submitting"
         />
+
+        <p class="center mt-8 text-sm italic text-gray-500">Ou alors:</p>
+        <a
+          href="https://discord.gg/4GK7rK4UTC"
+          type="button"
+          class="ml-14 mt-4 mr-14 mb-2 inline-flex items-center rounded-lg bg-[#050708] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#050708]/90 focus:outline-none focus:ring-4 focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-[#050708]/50"
+        >
+          <img class="mr-2" width="24" src="../../assets/discord.png" alt="logo discord">
+          &nbsp Contactez nous sur discord
+        </a>
       </div>
     </form>
   </AuthView>
 </template>
-
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue'
 import { databaseClient } from '@/database/implementation'
@@ -52,7 +56,6 @@ import InputField from '@/components/style/InputField.vue'
 import AuthView from '../../views/auth/AuthView.vue'
 import SubmitButton from '@/components/style/SubmitButton.vue'
 import TextArea from '@/components/style/TextArea.vue'
-import { isEmail } from '@/utils/misc_utils'
 import {
   MessageReplacer,
   MessageStack,
@@ -60,23 +63,23 @@ import {
 } from '@/views/messages/message_stack'
 import type { Message } from '@/views/messages/message_stack'
 import { useRouter } from 'vue-router'
-
 function goHome() {
   router.push('/')
 }
-
+function isEmail(email: string) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    )
+}
 const router = useRouter()
-
 const name = ref('')
 const email = ref('')
 const message = ref('')
-
 const emailError = ref('')
-
 const submitting = ref(false)
-
 let replacer = new MessageReplacer()
-
 watch(email, (value) => {
   if (value && !isEmail(value)) {
     emailError.value = "Cette adresse email n'est pas valide"
@@ -84,22 +87,17 @@ watch(email, (value) => {
     emailError.value = ''
   }
 })
-
 const invalidFields = computed(() => {
   return email.value == '' || emailError.value != ''
 })
-
 async function tryRegister() {
   console.log('Try to send message')
-
   replacer.replaceLastBy({
     type: MessageType.INFO,
     text: 'Envoie en cours...',
     timeout: 5000,
   })
-
   submitting.value = true
-
   await databaseClient
     .RecieveMessage(name.value, email.value, message.value)
     .then(() => {
@@ -108,11 +106,9 @@ async function tryRegister() {
         text: 'Votre message a correctement été envoyé.',
         timeout: 5000,
       })
-
       email.value = ''
       name.value = ''
       message.value = ''
-
       goHome()
     })
     .catch((error) => {
